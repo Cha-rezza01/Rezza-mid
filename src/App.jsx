9c79a4921538f200c8fce13d383c04dbe87ce4e3
Rezza <email@domain.com>
2024-11-19 13:39:37 +0800

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import './App.css'
-import Home from './pages/Home'
-import DetailPortfolio  from './pages/DetailPortfolio'
+import './App.css';
+import Home from './pages/Home';
+import DetailPortfolio from './pages/DetailPortfolio';
 import PageNotFound from './pages/PageNotFound';
-import Education from './pages/Education'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Education from './pages/Education';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='' element={<Home />} />
-        <Route path='/portfolio/:id' element={<DetailPortfolio />} />
-        <Route path='/page-not-found' element={<PageNotFound />} />
-        <Route path='/education' element={<Education/>} />
-        <Route path='' element={<PageNotFound />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/portfolio/:id" element={<DetailPortfolio />} />
+        <Route path="/education" element={<Education />} />
+        
+        {/* Route wildcard untuk halaman yang tidak ditemukan */}
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
