0529f89151ef117d4203a346592c907e1f5a583d
Rezza <email@domain.com>
2024-11-19 13:35:09 +0800

    edit

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
+// Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyANlW71LFMnXEr4gSWbgfqxrxAUvYcNyA0",
   authDomain: "cv-rezza.firebaseapp.com",
@@ -8,13 +9,16 @@ const firebaseConfig = {
   storageBucket: "cv-rezza.firebasestorage.app",
   messagingSenderId: "132462907909",
   appId: "1:132462907909:web:ba919806a7bd3fb79d026c",
-  measurementId: "G-GZK82T1LT9",
+  measurementId: "G-GZK82T1LT9"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Ensure Analytics is only initialized in the browser
 let analytics;
 if (typeof window !== "undefined") {
   analytics = getAnalytics(app);
 }
+
+export { analytics };
