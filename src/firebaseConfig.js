d9dbaf600680db50760dd7c6398a9c14d7c35f15
Rezza <email@domain.com>
2024-11-19 13:20:45 +0800

    Normalize line endings

---
+++ src/firebaseConfig.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyANlW71LFMnXEr4gSWbgfqxrxAUvYcNyA0",
+  authDomain: "cv-rezza.firebaseapp.com",
+  projectId: "cv-rezza",
+  storageBucket: "cv-rezza.firebasestorage.app",
+  messagingSenderId: "132462907909",
+  appId: "1:132462907909:web:ba919806a7bd3fb79d026c",
+  measurementId: "G-GZK82T1LT9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+let analytics;
+if (typeof window !== "undefined") {
+  analytics = getAnalytics(app);
+}
